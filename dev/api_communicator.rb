d91c3cb72454853f06e7eab3cdf318b8127e218c
alexneustein <alex.neustein@gmail.com>
2018-07-10 14:14:26 -0400

    updated master list builder

--- dev/api_communicator.rb
+++ dev/api_communicator.rb
@@ -2,74 +2,50 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+# These methods are used to scrape the drink instructions and ingredients from the cocktail DB site.
 
+cocktail_array = ["Martini","Manhattan","Old Fashioned","Mint Julep","Mojito","Margarita","Daiquiri","Tom Collins","Martinez","Brandy Cocktail","Brandy Daisy","Sidecar","Whiskey Sour","Sazerac","New Orleans Fizz","French 75","Negroni","Brandy Alexander","Bronx Cocktail"]
 
-def get_drink(drink)
+def get_drink_hash(drink)
   #gets the drink hash
   all_drinks = RestClient.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + drink.to_s)
-  drink_hash = JSON.parse(all_drinks)
-  selected_drink = drink_hash["drinks"][0]
-  binding.pry
+  wrapper_hash = JSON.parse(all_drinks)
+  wrapper_hash["drinks"].kind_of?(Array) ? drink_hash = wrapper_hash["drinks"][0] : drink_hash = wrapper_hash["drinks"]
 end
 
-get_drink("margarita")
+def get_ingredients(drink_hash)
+  return [] if drink_hash == nil
+  ingredient_array = drink_hash.fetch_values("strIngredient1",
+ "strIngredient2",
+ "strIngredient3",
+ "strIngredient4",
+ "strIngredient5",
+ "strIngredient6",
+ "strIngredient7",
+ "strIngredient8",
+ "strIngredient9",
+ "strIngredient10",
+ "strIngredient11",
+ "strIngredient12",
+ "strIngredient13",
+ "strIngredient14",
+ "strIngredient15")
+ ingredient_array.delete("")
+ return ingredient_array
+end
 
-# def get_character_movies_from_api(character)
-#   found_character_hash = get_character_hash_from_api(character)
-#
-#   movies_array = []
-#
-#   while found_character_hash == nil
-#     puts "Character Not Found. Please put a valid character."
-#     character = get_character_from_user
-#     found_character_hash = get_character_hash_from_api(character)
-#   end
-#
-#   the_URLs_array = found_character_hash["films"]
-#   the_URLs_array.each do |url|
-#   movies_array << JSON.parse(RestClient.get(url))
-#
-# end
-# return movies_array
-#   # iterate over the character hash to find the collection of `films` for the given
-#   #   `character`
-#   # collect those film API urls, make a web request to each URL to get the info
-#   #  for that film
-#   # return value of this method should be collection of info about each film.
-#   #  i.e. an array of hashes in which each hash reps a given film
-#   # this collection will be the argument given to `parse_character_movies`
-#   #  and that method will do some nice presentation stuff: puts out a list
-#   #  of movies by title. play around with puts out other info about a given film.
-# end
-#
-#
-# def parse_character_movies(films_hash)
-#   relevant_stuff = ["title", "episode id", "producer", "director", "release date"]
-#
-#   films_hash.map do |movie_hash|
-#     movie_hash.map do |key, detail|
-#       relevant_stuff.each do |category|
-#         if key == category
-#           print key.sub("_", " ")
-#           print ": "
-#           puts detail
-#           puts "" if key == "producer"
-#         end
-#       end
-#     end
-#   end
-# end
-#
-# # Luke_movie_hash = get_character_movies_from_api("Luke Skywalker")
-# # parse_character_movies(Luke_movie_hash)
-#
-#
-# def show_character_movies(character)
-#   films_hash = get_character_movies_from_api(character)
-#   parse_character_movies(films_hash)
-# end
+def get_instructions(drink_hash)
+  instruction_string = drink_hash["strInstructions"]
+end
 
-## BONUS
+def build_master_list(drink_names_array)
+  master_ingredient_list = []
+  drink_names_array.each do |drink_name|
+    master_ingredient_list << get_ingredients(get_drink_hash(drink_name)).compact
+    master_ingredient_list = master_ingredient_list.flatten
+    master_ingredient_list = master_ingredient_list.uniq
+  end
+  master_ingredient_list
+end
 
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
+pry.start
