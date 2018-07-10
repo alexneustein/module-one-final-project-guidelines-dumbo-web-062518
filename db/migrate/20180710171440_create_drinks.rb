79e7ae3d2668da3540517db4d348513f70c8796a
alexneustein <alex.neustein@gmail.com>
2018-07-10 15:40:26 -0400

    added interface script and removed one attribute from drink

--- db/migrate/20180710171440_create_drinks.rb
+++ db/migrate/20180710171440_create_drinks.rb
@@ -3,7 +3,6 @@ class CreateDrinks < ActiveRecord::Migration[5.0]
     create_table :drinks do |t|
       t.string :name
       t.string :instructions
-      t.integer :drinkingredient_id
     end
   end
 end
