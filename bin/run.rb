12e49f7491dc8373818517d8426476878236d405
alexneustein <alex.neustein@gmail.com>
2018-07-11 14:06:57 -0400

    completed joiner creation method

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,13 @@
 require_relative '../config/environment'
 
+cocktail_array = ["Martini","Manhattan","Old Fashioned","Mint Julep","Mojito","Margarita","Daiquiri","Tom Collins","Martinez","Brandy Cocktail","Brandy Daisy","Sidecar","Whiskey Sour","Sazerac","New Orleans Fizz","French 75","Negroni","Brandy Alexander","Bronx Cocktail"]
 
+
+# cocktail_array.each do |cocktail|
+#   make_joiner_entries(Drink, Ingredient, get_drink_id_and_ingredient_ids(cocktail))
+# end
+
+binding.pry
 
 puts "HELLO WORLD"
 
@@ -12,6 +19,6 @@ puts "HELLO WORLD"
 # # dryvermouth = Ingredient.create(name: "Dry Vermouth", category: "Liqueur", keyword: "Vermouth")
 #
 # # placeholder = DrinkIngredient.create(drink_id: 1, ingredient_id: 1)
-# 
+#
 # martini.ingredients << gin
 # martini.ingredients << dryvermouth
--- lib/Ingredient.rb
+++ lib/Ingredient.rb
@@ -3,4 +3,8 @@ class Ingredient < ActiveRecord::Base
   has_many :drinks, through: :drink_ingredients
   has_many :user_pantrys
   has_many :users, through: :user_pantrys
+
+  def self.id_from_name(name_string)
+    self.find_by(name: name_string).id
+  end
 end
